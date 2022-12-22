4020048dd6d78ffb17f96044cc67b21217a3cf6e
YashashreeT12 <yashashree12me@gmail.com>
2022-12-23 01:25:42 +0530

    express Started

--- src/express1.js
+++ src/express1.js
@@ -2,5 +2,7 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => res.send("Hello Yashu"));
-
+app.get("/explore", (req, res) => {
+  res.send("Hello Yashu 2");
+});
 app.listen(3000);
